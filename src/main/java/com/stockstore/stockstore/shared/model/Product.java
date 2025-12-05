5b859ed958d96fc33b67545e2df284eaaf53c280
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2025-12-05 16:02:39 -0300

    Producto Model

--- src/main/java/com/stockstore/stockstore/shared/model/Product.java
+++ src/main/java/com/stockstore/stockstore/shared/model/Product.java
@@ -1,4 +1,44 @@
 package com.stockstore.stockstore.shared.model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "productos")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String imageurl;
+
+    @OneToMany(mappedBy = "product")
+    private List<InventoryItem> inventoryItems =  new ArrayList<>();
+
+    @OneToMany(mappedBy = "product")
+    private List<Order> orders =  new ArrayList<>();
+
+    @ManyToMany(mappedBy = "products")
+    private List<Category> categories =new ArrayList<>();
+
+   @Builder
+    public Product(String name, String imageurl) {
+        this.name = name;
+        this.imageurl = imageurl;
+    }
+
+
+
 }
