c7666dadc6a8ecccf2cbc4cf2e3fbabc3ed82c7e
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 15:05:06 -0300

    Add InventoryItemMapper

--- src/main/java/com/stockstore/stockstore/shared/mapper/InventoryItemMapper.java
+++ src/main/java/com/stockstore/stockstore/shared/mapper/InventoryItemMapper.java
@@ -1,4 +1,15 @@
 package com.stockstore.stockstore.shared.mapper;
 
+import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemDetailDTO;
+import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemRequestDTO;
+import com.stockstore.stockstore.shared.model.InventoryItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
 public interface InventoryItemMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "product", ignore = true)
+    InventoryItem toEntity(InventoryItemRequestDTO dto);
+    InventoryItemDetailDTO toDetailDTO(InventoryItem entity);
 }
