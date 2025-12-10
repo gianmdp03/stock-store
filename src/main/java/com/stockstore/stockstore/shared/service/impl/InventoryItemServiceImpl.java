01a6aad0f2e3ec13e2369bebd79b33f5958aa485
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2025-12-10 18:13:15 -0300

    addInventoryItem implementend in service

--- src/main/java/com/stockstore/stockstore/shared/service/impl/InventoryItemServiceImpl.java
+++ src/main/java/com/stockstore/stockstore/shared/service/impl/InventoryItemServiceImpl.java
@@ -1,9 +1,13 @@
 package com.stockstore.stockstore.shared.service.impl;
 
+import com.stockstore.stockstore.exception.NotFoundException;
 import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemDetailDTO;
 import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemRequestDTO;
 import com.stockstore.stockstore.shared.mapper.InventoryItemMapper;
+import com.stockstore.stockstore.shared.model.InventoryItem;
+import com.stockstore.stockstore.shared.model.Product;
 import com.stockstore.stockstore.shared.repository.InventoryItemRepository;
+import com.stockstore.stockstore.shared.repository.ProductRepository;
 import com.stockstore.stockstore.shared.service.InventoryItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,16 +21,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class InventoryItemServiceImpl implements InventoryItemService {
     private final InventoryItemRepository inventoryItemRepository;
     private final InventoryItemMapper inventoryItemMapper;
+    private final ProductRepository productRepository;
 
     @Override
     @Transactional
     public InventoryItemDetailDTO addInventoryItem(InventoryItemRequestDTO dto) {
-       Product
+        Product product = productRepository.findById(dto.productId())
+                .orElseThrow(()-> new NotFoundException("Product not found"));
+        InventoryItem inventoryItem = inventoryItemRepository.save(inventoryItemMapper.toEntity(dto));
+        return inventoryItemMapper.toDetailDTO(inventoryItem);
     }
 
     @Override
     public Page<InventoryItemDetailDTO> listInventoryItems(Pageable pageable) {
-        return null;
+       Page<Inven>
     }
 
     @Override
