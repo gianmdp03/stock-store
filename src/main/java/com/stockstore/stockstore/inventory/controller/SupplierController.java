d118416ea3fbbf5c2a804ae402e613f317919106
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 16:46:53 -0300

    Add SupplierController

--- src/main/java/com/stockstore/stockstore/inventory/controller/SupplierController.java
+++ src/main/java/com/stockstore/stockstore/inventory/controller/SupplierController.java
@@ -1,13 +1,39 @@
 package com.stockstore.stockstore.inventory.controller;
 
+import com.stockstore.stockstore.inventory.dto.supplier.SupplierDetailDTO;
+import com.stockstore.stockstore.inventory.dto.supplier.SupplierListDTO;
+import com.stockstore.stockstore.inventory.dto.supplier.SupplierRequestDTO;
 import com.stockstore.stockstore.inventory.service.SupplierService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/suppliers")
 @RequiredArgsConstructor
 public class SupplierController {
     private final SupplierService supplierService;
+
+    @PostMapping
+    public ResponseEntity<SupplierDetailDTO> addSupplier(@Valid @RequestBody SupplierRequestDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(supplierService.addSupplier(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<SupplierListDTO>> listSuppliers(
+            @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(supplierService.listSuppliers(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSupplier(@PathVariable Long id){
+        supplierService.deleteSupplier(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
