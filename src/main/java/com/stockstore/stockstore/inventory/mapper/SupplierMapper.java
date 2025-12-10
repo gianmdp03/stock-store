ffdc0ae4ad2838313108fc750d7ab8ead92c6105
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 15:21:35 -0300

    Add SupplierMapper

--- src/main/java/com/stockstore/stockstore/inventory/mapper/SupplierMapper.java
+++ src/main/java/com/stockstore/stockstore/inventory/mapper/SupplierMapper.java
@@ -1,4 +1,18 @@
 package com.stockstore.stockstore.inventory.mapper;
 
+import com.stockstore.stockstore.inventory.dto.supplier.SupplierDetailDTO;
+import com.stockstore.stockstore.inventory.dto.supplier.SupplierListDTO;
+import com.stockstore.stockstore.inventory.dto.supplier.SupplierRequestDTO;
+import com.stockstore.stockstore.inventory.model.Supplier;
+import com.stockstore.stockstore.shared.mapper.ProductMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
 public interface SupplierMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "products", ignore = true)
+    Supplier toEntity(SupplierRequestDTO dto);
+    SupplierDetailDTO toDetailDto(Supplier entity);
+    SupplierListDTO toListDto(Supplier entity);
 }
