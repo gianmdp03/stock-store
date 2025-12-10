2b2c0beddb160e7840bc12d2053e7dcaec976469
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 15:09:21 -0300

    Add OrderMapper

--- src/main/java/com/stockstore/stockstore/shared/mapper/OrderMapper.java
+++ src/main/java/com/stockstore/stockstore/shared/mapper/OrderMapper.java
@@ -1,4 +1,17 @@
 package com.stockstore.stockstore.shared.mapper;
 
+import com.stockstore.stockstore.shared.dto.order.OrderDetailDTO;
+import com.stockstore.stockstore.shared.dto.order.OrderListDTO;
+import com.stockstore.stockstore.shared.dto.order.OrderRequestDTO;
+import com.stockstore.stockstore.shared.model.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
 public interface OrderMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "products", ignore = true)
+    Order toEntity(OrderRequestDTO dto);
+    OrderDetailDTO toDetailDto(Order order);
+    OrderListDTO toListDto(Order order);
 }
