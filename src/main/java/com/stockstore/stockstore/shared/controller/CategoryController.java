d931157976192a610eebf916146166b5d449c80f
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 16:54:14 -0300

    Add CategoryController

--- src/main/java/com/stockstore/stockstore/shared/controller/CategoryController.java
+++ src/main/java/com/stockstore/stockstore/shared/controller/CategoryController.java
@@ -1,13 +1,39 @@
 package com.stockstore.stockstore.shared.controller;
 
+import com.stockstore.stockstore.shared.dto.category.CategoryDetailDTO;
+import com.stockstore.stockstore.shared.dto.category.CategoryListDTO;
+import com.stockstore.stockstore.shared.dto.category.CategoryRequestDTO;
 import com.stockstore.stockstore.shared.service.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDetailDTO> addCategory(@Valid @RequestBody CategoryRequestDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.addCategory(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CategoryListDTO>> listCategories(
+            @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.listCategories(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id){
+        categoryService.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
