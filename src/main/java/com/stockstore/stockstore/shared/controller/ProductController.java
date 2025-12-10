c95259bf2b55b9ef197f9a98c0cc855794fe3d4a
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 17:11:15 -0300

    Add ProductController

--- src/main/java/com/stockstore/stockstore/shared/controller/ProductController.java
+++ src/main/java/com/stockstore/stockstore/shared/controller/ProductController.java
@@ -1,13 +1,39 @@
 package com.stockstore.stockstore.shared.controller;
 
+import com.stockstore.stockstore.shared.dto.product.ProductDetailDTO;
+import com.stockstore.stockstore.shared.dto.product.ProductListDTO;
+import com.stockstore.stockstore.shared.dto.product.ProductRequestDTO;
 import com.stockstore.stockstore.shared.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductDetailDTO> addProduct(@Valid @RequestBody ProductRequestDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.addProduct(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductListDTO>> listProducts(
+            @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(productService.listProducts(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(Long id){
+        productService.deleteProduct(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
