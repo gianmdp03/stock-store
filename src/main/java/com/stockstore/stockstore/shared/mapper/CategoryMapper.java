73b33f43ce88095fd77259592cb03f1f868691b2
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 14:40:19 -0300

    Add CategoryMapper

--- src/main/java/com/stockstore/stockstore/shared/mapper/CategoryMapper.java
+++ src/main/java/com/stockstore/stockstore/shared/mapper/CategoryMapper.java
@@ -1,4 +1,17 @@
 package com.stockstore.stockstore.shared.mapper;
 
+import com.stockstore.stockstore.shared.dto.category.CategoryDetailDTO;
+import com.stockstore.stockstore.shared.dto.category.CategoryListDTO;
+import com.stockstore.stockstore.shared.dto.category.CategoryRequestDTO;
+import com.stockstore.stockstore.shared.model.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring",  uses = {ProductMapper.class})
 public interface CategoryMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "products", ignore = true)
+    Category toEntity(CategoryRequestDTO dto);
+    CategoryDetailDTO toDetailDto(Category entity);
+    CategoryListDTO toListDto(Category entity);
 }
