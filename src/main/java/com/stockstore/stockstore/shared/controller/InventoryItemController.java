238e20c88bfc2077698066c29262f6a2e98b8ad4
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 17:06:29 -0300

    Add OrderController + fix InventoryItemController

--- src/main/java/com/stockstore/stockstore/shared/controller/InventoryItemController.java
+++ src/main/java/com/stockstore/stockstore/shared/controller/InventoryItemController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +25,8 @@ public class InventoryItemController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<InventoryItemDetailDTO>> listInventoryItems(Pageable pageable){
+    public ResponseEntity<Page<InventoryItemDetailDTO>> listInventoryItems(
+            @PageableDefault(page = 0, size = 10, sort = "expireDate", direction = Sort.Direction.DESC) Pageable pageable){
         return ResponseEntity.status(HttpStatus.OK).body(inventoryItemService.listInventoryItems(pageable));
     }
 
--- src/main/java/com/stockstore/stockstore/shared/controller/OrderController.java
+++ src/main/java/com/stockstore/stockstore/shared/controller/OrderController.java
@@ -1,13 +1,40 @@
 package com.stockstore.stockstore.shared.controller;
 
+import com.stockstore.stockstore.shared.dto.order.OrderDetailDTO;
+import com.stockstore.stockstore.shared.dto.order.OrderListDTO;
+import com.stockstore.stockstore.shared.dto.order.OrderRequestDTO;
 import com.stockstore.stockstore.shared.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderDetailDTO> addOrder(@Valid @RequestBody OrderRequestDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.addOrder(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<OrderListDTO>> listOrders(
+            @PageableDefault(page = 0, size = 10, sort = "saleDate", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.listOrders(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id){
+        orderService.deleteOrder(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
