75a53dae74a2b301e219660a201f5805e3498326
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-10 16:58:37 -0300

    Add InventoryItemController

--- src/main/java/com/stockstore/stockstore/shared/controller/InventoryItemController.java
+++ src/main/java/com/stockstore/stockstore/shared/controller/InventoryItemController.java
@@ -1,13 +1,35 @@
 package com.stockstore.stockstore.shared.controller;
 
+import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemDetailDTO;
+import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemRequestDTO;
 import com.stockstore.stockstore.shared.service.InventoryItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inventory-items")
 @RequiredArgsConstructor
 public class InventoryItemController {
     private final InventoryItemService inventoryItemService;
+
+    @PostMapping
+    public ResponseEntity<InventoryItemDetailDTO> addInventoryItem(@Valid @RequestBody InventoryItemRequestDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(inventoryItemService.addInventoryItem(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<InventoryItemDetailDTO>> listInventoryItems(Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(inventoryItemService.listInventoryItems(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteInventoryItem(@PathVariable Long id){
+        inventoryItemService.deleteInventoryItem(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
