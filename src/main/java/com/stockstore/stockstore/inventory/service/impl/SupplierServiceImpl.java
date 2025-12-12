baaab2e41bd8540fa8eb399caa9bc41cc80a6230
Joaquin <joaco.rodriguezortega@gmail.com>
2025-12-11 21:02:08 -0300

    SupplierServiceImpl methods

--- src/main/java/com/stockstore/stockstore/inventory/service/impl/SupplierServiceImpl.java
+++ src/main/java/com/stockstore/stockstore/inventory/service/impl/SupplierServiceImpl.java
@@ -1,11 +1,15 @@
 package com.stockstore.stockstore.inventory.service.impl;
 
+import com.stockstore.stockstore.exception.NotFoundException;
 import com.stockstore.stockstore.inventory.dto.supplier.SupplierDetailDTO;
 import com.stockstore.stockstore.inventory.dto.supplier.SupplierListDTO;
 import com.stockstore.stockstore.inventory.dto.supplier.SupplierRequestDTO;
 import com.stockstore.stockstore.inventory.mapper.SupplierMapper;
+import com.stockstore.stockstore.inventory.model.Supplier;
 import com.stockstore.stockstore.inventory.repository.SupplierRepository;
 import com.stockstore.stockstore.inventory.service.SupplierService;
+import com.stockstore.stockstore.shared.model.Category;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,17 +26,27 @@ public class SupplierServiceImpl implements SupplierService {
     @Override
     @Transactional
     public SupplierDetailDTO addSupplier(SupplierRequestDTO dto) {
-        return null;
+        Supplier supplier = supplierMapper.toEntity(dto);
+        Supplier savedSupplier = supplierRepository.save(supplier);
+
+        return supplierMapper.toDetailDto(savedSupplier);
     }
 
     @Override
     public Page<SupplierListDTO> listSuppliers(Pageable pageable) {
-        return null;
+        Page<Supplier> supplierPage = supplierRepository.findAll(pageable);
+        if(supplierPage.isEmpty()){
+            throw new NotFoundException("List is empty");
+        }
+
+        return supplierPage.map(supplierMapper::toListDto);
     }
 
     @Override
     @Transactional
     public void deleteSupplier(Long supplierId) {
+        Supplier supplier = supplierRepository.findById(supplierId).orElseThrow(()-> new NotFoundException("Supplier ID does not exist"));
 
+        supplierRepository.delete(supplier);
     }
 }
