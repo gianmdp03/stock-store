4b8fecc62b0032328fdc920aced3b7b17c213769
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-12 18:06:32 -0300

    Update OrderItemDetailDTO.java

--- src/main/java/com/stockstore/stockstore/shared/dto/orderItem/OrderItemDetailDTO.java
+++ src/main/java/com/stockstore/stockstore/shared/dto/orderItem/OrderItemDetailDTO.java
@@ -2,5 +2,5 @@ package com.stockstore.stockstore.shared.dto.orderItem;
 
 import com.stockstore.stockstore.shared.dto.product.ProductListDTO;
 
-public record OrderItemDetailDTO (Long id, ProductListDTO productListDTO, int amount){
+public record OrderItemDetailDTO (Long id, ProductListDTO product, int amount){
 }
