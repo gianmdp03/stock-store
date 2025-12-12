f8883d49290b7a04b6f77b4c86fc0660ec871b61
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2025-12-12 16:47:55 -0300

    OrderItem entity complete

--- src/main/java/com/stockstore/stockstore/shared/model/OrderItem.java
+++ src/main/java/com/stockstore/stockstore/shared/model/OrderItem.java
@@ -1,4 +1,31 @@
 package com.stockstore.stockstore.shared.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import lombok.Setter;
+
+@Entity
+@Table(name = "orderitem")
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int amount;
+
+    @ManyToOne
+    @JoinColumn(name = "order_item_id")
+    private Product product;
+
+    public OrderItem(int amount, Product product) {
+        this.amount = amount;
+        this.product = product;
+    }
+    
 }
