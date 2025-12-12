675b51093a091b0dc99a33d5846817cd67132a8b
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-12 15:24:42 -0300

    Add ProductServiceImpl

--- src/main/java/com/stockstore/stockstore/shared/service/impl/ProductServiceImpl.java
+++ src/main/java/com/stockstore/stockstore/shared/service/impl/ProductServiceImpl.java
@@ -1,9 +1,15 @@
 package com.stockstore.stockstore.shared.service.impl;
 
+import com.stockstore.stockstore.exception.NotFoundException;
+import com.stockstore.stockstore.inventory.model.Supplier;
+import com.stockstore.stockstore.inventory.repository.SupplierRepository;
 import com.stockstore.stockstore.shared.dto.product.ProductDetailDTO;
 import com.stockstore.stockstore.shared.dto.product.ProductListDTO;
 import com.stockstore.stockstore.shared.dto.product.ProductRequestDTO;
 import com.stockstore.stockstore.shared.mapper.ProductMapper;
+import com.stockstore.stockstore.shared.model.Category;
+import com.stockstore.stockstore.shared.model.Product;
+import com.stockstore.stockstore.shared.repository.CategoryRepository;
 import com.stockstore.stockstore.shared.repository.ProductRepository;
 import com.stockstore.stockstore.shared.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -12,27 +18,43 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final SupplierRepository supplierRepository;
+    private final CategoryRepository categoryRepository;
 
     @Override
     @Transactional
     public ProductDetailDTO addProduct(ProductRequestDTO dto) {
-        return null;
+        Supplier supplier = supplierRepository.findById(dto.supplierId()).orElseThrow(()-> new NotFoundException("Supplier ID does not exist"));
+        List<Category> categories = categoryRepository.findAllById(dto.categoriesId());
+        if(categories.isEmpty())
+            throw new NotFoundException("List is empty");
+        Product product = productMapper.toEntity(dto);
+        product.setSupplier(supplier);
+        product.setCategories(categories);
+        product = productRepository.save(product);
+        return productMapper.toDetailDto(product);
     }
 
     @Override
     public Page<ProductListDTO> listProducts(Pageable pageable) {
-        return null;
+        Page<Product> page = productRepository.findAll(pageable);
+        if (page.isEmpty())
+            throw new NotFoundException("List is empty");
+        return page.map(productMapper::toListDto);
     }
 
     @Override
     @Transactional
     public void deleteProduct(Long productId) {
-
+        Product product = productRepository.findById(productId).orElseThrow(()->new NotFoundException("Product ID does not exist"));
+        productRepository.delete(product);
     }
 }
