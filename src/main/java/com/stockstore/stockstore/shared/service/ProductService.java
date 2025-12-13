8605d7b0adafef483a967a35146b305a21693a05
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2025-12-13 17:33:18 -0300

    update product in ProductService

--- src/main/java/com/stockstore/stockstore/shared/service/ProductService.java
+++ src/main/java/com/stockstore/stockstore/shared/service/ProductService.java
@@ -3,11 +3,13 @@ package com.stockstore.stockstore.shared.service;
 import com.stockstore.stockstore.shared.dto.product.ProductDetailDTO;
 import com.stockstore.stockstore.shared.dto.product.ProductListDTO;
 import com.stockstore.stockstore.shared.dto.product.ProductRequestDTO;
+import com.stockstore.stockstore.shared.dto.product.ProductUpdateDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ProductService {
     ProductDetailDTO addProduct(ProductRequestDTO dto);
     Page<ProductListDTO> listProducts(Pageable pageable);
+    ProductDetailDTO updateProducto(Long id, ProductUpdateDTO dto);
     void deleteProduct(Long productId);
 }
