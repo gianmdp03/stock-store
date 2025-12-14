aec3376650d0630769680ec555733eed436d72c6
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-14 12:49:48 -0300

    Add updateEntityFromDto to SupplierMapper

--- src/main/java/com/stockstore/stockstore/inventory/mapper/SupplierMapper.java
+++ src/main/java/com/stockstore/stockstore/inventory/mapper/SupplierMapper.java
@@ -3,16 +3,21 @@ package com.stockstore.stockstore.inventory.mapper;
 import com.stockstore.stockstore.inventory.dto.supplier.SupplierDetailDTO;
 import com.stockstore.stockstore.inventory.dto.supplier.SupplierListDTO;
 import com.stockstore.stockstore.inventory.dto.supplier.SupplierRequestDTO;
+import com.stockstore.stockstore.inventory.dto.supplier.SupplierUpdateDTO;
 import com.stockstore.stockstore.inventory.model.Supplier;
 import com.stockstore.stockstore.shared.mapper.ProductMapper;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", uses = {ProductMapper.class})
-public interface SupplierMapper {
+public abstract class SupplierMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "products", ignore = true)
-    Supplier toEntity(SupplierRequestDTO dto);
-    SupplierDetailDTO toDetailDto(Supplier entity);
-    SupplierListDTO toListDto(Supplier entity);
+    public abstract Supplier toEntity(SupplierRequestDTO dto);
+    public abstract SupplierDetailDTO toDetailDto(Supplier entity);
+    public abstract SupplierListDTO toListDto(Supplier entity);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "products", ignore = true)
+    public abstract void updateEntityFromDto(SupplierUpdateDTO dto, @MappingTarget Supplier entity);
+
 }
