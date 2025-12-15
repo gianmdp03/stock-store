6dc08abed8641d149060e76cc0f0062ffbe0931b
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-15 16:18:35 -0300

    Add search method to categoryRepository

--- src/main/java/com/stockstore/stockstore/shared/repository/CategoryRepository.java
+++ src/main/java/com/stockstore/stockstore/shared/repository/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.stockstore.stockstore.shared.repository;
 
 import com.stockstore.stockstore.shared.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Page<Category> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
