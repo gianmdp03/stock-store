0ef7de3db0ac7c556dc550cbcc722b09b8b8b513
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-15 17:06:04 -0300

    Add searchOrders to controller

--- src/main/java/com/stockstore/stockstore/shared/controller/OrderController.java
+++ src/main/java/com/stockstore/stockstore/shared/controller/OrderController.java
@@ -1,11 +1,8 @@
 package com.stockstore.stockstore.shared.controller;
 
 import com.stockstore.stockstore.shared.dto.order.OrderDetailDTO;
-import com.stockstore.stockstore.shared.dto.order.OrderUpdateDTO;
 import com.stockstore.stockstore.shared.service.OrderService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -13,6 +10,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -31,14 +30,16 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.listOrders(pageable));
     }
 
+    @GetMapping("/{saleDate}")
+    public ResponseEntity<Page<OrderDetailDTO>> searchOrders(
+            @PathVariable LocalDate saleDate,
+            @PageableDefault(page = 0, size = 10, sort = "saleDate", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.searchOrders(saleDate, pageable));
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteOrder(@PathVariable Long id){
         orderService.deleteOrder(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity<OrderDetailDTO> updateOrder(@PathVariable Long id, @RequestBody @Valid OrderUpdateDTO dto){
-        return ResponseEntity.status(HttpStatus.OK).build(orderService.updateOrder(id, dto));
     }
 }
