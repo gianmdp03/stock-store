933ddc20e2907b735421ba51c0f703f68c58226d
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2025-12-15 16:51:33 -0300

    findByExpireDate in inventoryItem controller & Service

--- src/main/java/com/stockstore/stockstore/shared/service/InventoryItemService.java
+++ src/main/java/com/stockstore/stockstore/shared/service/InventoryItemService.java
@@ -3,12 +3,16 @@ package com.stockstore.stockstore.shared.service;
 import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemDetailDTO;
 import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemRequestDTO;
 import com.stockstore.stockstore.shared.dto.inventoryitem.InventoryItemUpdateDTO;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
 
 public interface InventoryItemService {
     InventoryItemDetailDTO addInventoryItem(InventoryItemRequestDTO dto);
     InventoryItemDetailDTO updateInventoryItem(Long id, InventoryItemUpdateDTO dto);
     Page<InventoryItemDetailDTO> listInventoryItems(Pageable pageable);
     void deleteInventoryItem(Long inventoryItemId);
+    Page<InventoryItemDetailDTO> searchInventoryItem(Pageable page, LocalDate date);
 }
