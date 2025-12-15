8e595e12e2b277da950c09c50be6cfd4f98fd4fd
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2025-12-15 16:26:49 -0300

    findByEmailContainingIgnoreCase in supplierService

--- src/main/java/com/stockstore/stockstore/inventory/repository/SupplierRepository.java
+++ src/main/java/com/stockstore/stockstore/inventory/repository/SupplierRepository.java
@@ -1,9 +1,14 @@
 package com.stockstore.stockstore.inventory.repository;
 
+import com.stockstore.stockstore.inventory.dto.supplier.SupplierListDTO;
 import com.stockstore.stockstore.inventory.model.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+    Page<Supplier> findByNameContainingIgnoreCase(String name, Pageable page);
+    Page<Supplier> findByEmailContainingIgnoreCase(String email, Pageable page);
 }
