02dcef5fe5ad9be7d53cab07bb0107a0ba89bbdf
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2025-12-17 14:35:24 -0300

    supplier email unique

--- src/main/java/com/stockstore/stockstore/inventory/model/Supplier.java
+++ src/main/java/com/stockstore/stockstore/inventory/model/Supplier.java
@@ -23,7 +23,7 @@ public class Supplier {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
