cb53fddf7de08ccab213e854fbac1647f1cb1471
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-17 16:17:07 -0300

    Add JwtAuthenticationFilter

--- src/main/java/com/stockstore/stockstore/security/config/ApplicationConfig.java
+++ src/main/java/com/stockstore/stockstore/security/config/ApplicationConfig.java
@@ -31,7 +31,7 @@ public class ApplicationConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
-    
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
