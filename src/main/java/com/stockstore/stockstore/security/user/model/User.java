333ca2a1cec2e6a45b5e6c345cdae3c727de4eef
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-17 15:18:39 -0300

    Add user model

--- src/main/java/com/stockstore/stockstore/security/user/model/User.java
+++ src/main/java/com/stockstore/stockstore/security/user/model/User.java
@@ -1,4 +1,43 @@
 package com.stockstore.stockstore.security.user.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    private String phoneNumber;
+
+    public User(String email, String username, String password, String name, String lastname, String phoneNumber) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.lastname = lastname;
+        this.phoneNumber = phoneNumber;
+    }
 }
