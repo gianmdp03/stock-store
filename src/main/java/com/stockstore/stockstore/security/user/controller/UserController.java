a3ed0648592e0e0a2b0312f0aae5a7fd2f1d83b9
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-17 15:32:56 -0300

    Add basic structure to user controller & service

--- src/main/java/com/stockstore/stockstore/security/user/controller/UserController.java
+++ src/main/java/com/stockstore/stockstore/security/user/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.stockstore.stockstore.security.user.controller;
 
+import com.stockstore.stockstore.security.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 }
--- src/main/java/com/stockstore/stockstore/security/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/stockstore/stockstore/security/user/service/impl/UserServiceImpl.java
@@ -1,4 +1,8 @@
 package com.stockstore.stockstore.security.user.service.impl;
 
-public class UserServiceImpl {
+import com.stockstore.stockstore.security.user.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
 }
