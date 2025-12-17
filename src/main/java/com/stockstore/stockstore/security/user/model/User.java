9dd8e30f131ed1f90e864101eeb3bdefd8a38ee9
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-17 15:57:09 -0300

    Implemented UserDetails in User

--- src/main/java/com/stockstore/stockstore/security/user/model/User.java
+++ src/main/java/com/stockstore/stockstore/security/user/model/User.java
@@ -1,16 +1,23 @@
 package com.stockstore.stockstore.security.user.model;
 
+import com.stockstore.stockstore.security.user.Enum.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,12 +39,41 @@ public class User {
 
     private String phoneNumber;
 
-    public User(String email, String username, String password, String name, String lastname, String phoneNumber) {
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User(String email, String username, String password, String name, String lastname, String phoneNumber, Role role) {
         this.email = email;
         this.username = username;
         this.password = password;
         this.name = name;
         this.lastname = lastname;
         this.phoneNumber = phoneNumber;
+        this.role = role;
     }
 }
