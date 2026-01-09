f581cd5a3f0ada7cd310a2650cb1bee9cc952f65
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-09 15:17:59 -0300

    Add EmailService & EmailServiceImpl

---
+++ src/main/java/com/stockstore/stockstore/inventory/service/impl/EmailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.stockstore.stockstore.inventory.service.impl;
+
+import com.stockstore.stockstore.inventory.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+    private final JavaMailSender javaMailSender;
+
+    @Value("{spring.mail.username}")
+    private String email;
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(email);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        javaMailSender.send(message);
+    }
+}
