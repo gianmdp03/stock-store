74b7cfd9971b08d7dc3eea05e3e68d9382d3b202
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-10 13:31:53 -0300

    Add LocalOrderController

--- src/main/java/com/stockstore/stockstore/local/controller/LocalOrderController.java
+++ src/main/java/com/stockstore/stockstore/local/controller/LocalOrderController.java
@@ -1,4 +1,30 @@
 package com.stockstore.stockstore.local.controller;
 
+import com.stockstore.stockstore.local.dto.localOrder.LocalOrderDetailDTO;
+import com.stockstore.stockstore.local.dto.localOrderItem.LocalOrderItemRequestDTO;
+import com.stockstore.stockstore.local.enums.PaymentMethod;
+import com.stockstore.stockstore.local.service.LocalOrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController("/api/local-orders")
+@RequiredArgsConstructor
 public class LocalOrderController {
+    private final LocalOrderService localOrderService;
+
+    @PostMapping("/{saleDate}/{paymentMethod}")
+    public ResponseEntity<LocalOrderDetailDTO> addLocalOrderWithItems(@PathVariable LocalDateTime saleDate,
+                                                                      @Valid @RequestBody List<LocalOrderItemRequestDTO> list,
+                                                                      @PathVariable PaymentMethod paymentMethod){
+        return ResponseEntity.status(HttpStatus.CREATED).body(localOrderService.addLocalOrderWithItems(saleDate, list, paymentMethod));
+    }
 }
