0b4452f0b731a585be57deb6f290aeb4381cc277
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-09 21:33:19 -0300

    Add LocalOrder (not finished) & LocalOrderItem

--- src/main/java/com/stockstore/stockstore/local/model/LocalOrder.java
+++ src/main/java/com/stockstore/stockstore/local/model/LocalOrder.java
@@ -1,5 +1,33 @@
 package com.stockstore.stockstore.local.model;
 
-public class LocalOrder {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "local_orders")
+@NoArgsConstructor
+public class LocalOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime saleDate;
+
+    @OneToMany(mappedBy = "localOrder")
+    private List<LocalOrderItem> localOrderItems;
+
+    //Metodo pago
+
+    public LocalOrder(LocalDateTime saleDate, List<LocalOrderItem> localOrderItems) {
+        this.saleDate = saleDate;
+        this.localOrderItems = localOrderItems;
+    }
 }
--- src/main/java/com/stockstore/stockstore/local/model/LocalOrderItem.java
+++ src/main/java/com/stockstore/stockstore/local/model/LocalOrderItem.java
@@ -1,4 +1,35 @@
 package com.stockstore.stockstore.local.model;
 
+import com.stockstore.stockstore.shared.model.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "local_order_items")
+@NoArgsConstructor
 public class LocalOrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private int amount;
+
+    @ManyToOne
+    @JoinColumn(name = "local_order_id")
+    private LocalOrder localOrder;
+
+    public LocalOrderItem(Product product, int amount, LocalOrder localOrder) {
+        this.product = product;
+        this.amount = amount;
+        this.localOrder = localOrder;
+    }
 }
