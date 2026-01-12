d81b1a9fc720d54b487490bf0115ba111a8c07c4
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-12 17:30:05 -0300

    Update LocalOrderItemDetailDTO.java

--- src/main/java/com/stockstore/stockstore/local/dto/localOrderItem/LocalOrderItemDetailDTO.java
+++ src/main/java/com/stockstore/stockstore/local/dto/localOrderItem/LocalOrderItemDetailDTO.java
@@ -2,4 +2,6 @@ package com.stockstore.stockstore.local.dto.localOrderItem;
 
 import com.stockstore.stockstore.shared.dto.product.ProductListDTO;
 
-public record LocalOrderItemDetailDTO(Long id, ProductListDTO product, int quantity) {}
+import java.math.BigDecimal;
+
+public record LocalOrderItemDetailDTO(Long id, ProductListDTO product, int quantity, BigDecimal price) {}
