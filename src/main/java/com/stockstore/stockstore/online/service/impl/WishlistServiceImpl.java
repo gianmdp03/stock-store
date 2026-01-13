679373b50d4fcdfd577a09c3cb82d16561667333
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-13 15:49:49 -0300

    Update WishlistServiceImpl.java

--- src/main/java/com/stockstore/stockstore/online/service/impl/WishlistServiceImpl.java
+++ src/main/java/com/stockstore/stockstore/online/service/impl/WishlistServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -57,7 +58,8 @@ public class WishlistServiceImpl implements WishlistService {
     @Override
     @Transactional
     public void deleteItemFromWishlist(String email, Long productId) {
-        User user = userRepository.findByEmail(email).orElseThrow(()-> new NotFoundException("Invalid Wishlist"));
-        wishlistRepository.deleteByProductIdAndUser(productId, user);
+        Wishlist wishlist = wishlistRepository.findByUserEmail(email).orElseThrow(()-> new NotFoundException("Invalid Wishlist or User"));
+        wishlist.getProducts().removeIf(product -> product.getId().equals(productId));
+
     }
 }
