244f30c93d88b97a5b910e52777e64e50e74a21a
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-16 19:35:48 -0300

    Update CartItemListDTO.java

--- src/main/java/com/stockstore/stockstore/online/dto/cartItem/CartItemListDTO.java
+++ src/main/java/com/stockstore/stockstore/online/dto/cartItem/CartItemListDTO.java
@@ -2,4 +2,4 @@ package com.stockstore.stockstore.online.dto.cartItem;
 
 import com.stockstore.stockstore.shared.dto.product.ProductListDTO;
 
-public record CartItemListDTO (ProductListDTO product, int quantity){}
+public record CartItemListDTO (Long id, ProductListDTO product, int quantity){}
