572ac6fa7c3c91fd71bc47589987924fdcef34fb
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-18 11:41:13 -0300

    Update AddressMapper.java

--- src/main/java/com/stockstore/stockstore/online/mapper/AddressMapper.java
+++ src/main/java/com/stockstore/stockstore/online/mapper/AddressMapper.java
@@ -1,10 +1,21 @@
 package com.stockstore.stockstore.online.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import com.stockstore.stockstore.online.dto.address.AddressDetailDTO;
+import com.stockstore.stockstore.online.dto.address.AddressRequestDTO;
+import com.stockstore.stockstore.online.dto.address.AddressUpdateDTO;
+import com.stockstore.stockstore.online.model.Address;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public abstract class AddressMapper {
     @Mapping(target = "id", ignore = true)
-
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
+    public abstract Address toEntity(AddressRequestDTO dto);
+    public abstract AddressDetailDTO toDetailDto(Address entity);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
+    public abstract void updateEntityFromDto(AddressUpdateDTO dto, @MappingTarget Address entity);
 }
