440dddbc66fa28d17b52d8335493f365147544bf
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-18 11:50:25 -0300

    Update AddressRepository.java

--- src/main/java/com/stockstore/stockstore/online/repository/AddressRepository.java
+++ src/main/java/com/stockstore/stockstore/online/repository/AddressRepository.java
@@ -1,9 +1,16 @@
 package com.stockstore.stockstore.online.repository;
 
 import com.stockstore.stockstore.online.model.Address;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    Optional<Address> findByNameAndUserEmail(String name, String email);
+    Page<Address> findByUserEmailAndEnabledTrue(String email, Pageable pageable);
+    Optional<Address> findByIdAndUserEmailAndEnabledTrue(Long id, String email);
 }
