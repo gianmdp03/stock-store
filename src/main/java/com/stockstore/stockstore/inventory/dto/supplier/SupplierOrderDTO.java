08a6f10c22f68f5e03b064a520f61dbcab81b787
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-02-04 19:00:13 -0300

    change in supplierorderdto

--- src/main/java/com/stockstore/stockstore/inventory/dto/supplier/SupplierOrderDTO.java
+++ src/main/java/com/stockstore/stockstore/inventory/dto/supplier/SupplierOrderDTO.java
@@ -4,5 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record SupplierOrderDTO(@NotBlank Long productId, @NotNull @Positive int quantity){
+public record SupplierOrderDTO(@NotNull(message = "El ID del producto es obligatorio")
+                               @Positive(message = "El ID debe ser mayor a 0")
+                               Long productId,
+
+                               @NotNull
+                               @Positive
+                               int quantity){
 }
