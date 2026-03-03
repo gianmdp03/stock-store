7f912b95f35e5610ddef853752b56428645e571c
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-03-03 17:46:31 -0300

    Update ProductListDTO.java

--- src/main/java/com/stockstore/stockstore/shared/dto/product/ProductListDTO.java
+++ src/main/java/com/stockstore/stockstore/shared/dto/product/ProductListDTO.java
@@ -5,5 +5,5 @@ import com.stockstore.stockstore.shared.dto.category.CategoryListDTO;
 import java.math.BigDecimal;
 import java.util.List;
 
-public record ProductListDTO(Long id, String name, String imageUrl, BigDecimal price, List<CategoryListDTO> categories, int stock){
+public record ProductListDTO(Long id, String name, String imageUrl, BigDecimal price, List<CategoryListDTO> categories){
 }
